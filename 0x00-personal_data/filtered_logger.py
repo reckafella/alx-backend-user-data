9887becc739c0a56b096c3bbef0af446d360f721
reckafella <ethanmuthoniw6@gmail.com>
2024-01-13 18:28:57 +0300

    fix code error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,10 +3,10 @@
 import re
 
 
-def filter_datum(fields: str, redaction: str, message: str, separator: str)\
-        -> str:
+def filter_datum(fields: str, redaction: str,
+                 message: str, separator: str) -> str:
     ''' Returns the log message obfuscated '''
     for field in fields:
-        repl = f'{field}={redaction}{separator}'
-        message = re.sub(f'{field}=.*?{separator}', repl, message)
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
