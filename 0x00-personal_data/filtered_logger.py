2bef76feca9857f07540f578e8701e760de377af
reckafella <ethanmuthoniw6@gmail.com>
2024-01-13 20:22:32 +0300

    add get_logger function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,8 @@ from typing import List
 import logging
 
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     '''
@@ -51,3 +53,18 @@ class RedactingFormatter(logging.Formatter):
         '''
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    '''
+    Returns:
+        a logging.Logger object
+    '''
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+
+    return logger
