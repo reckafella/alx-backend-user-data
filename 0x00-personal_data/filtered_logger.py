971d66173cd14a160519a749814009a23d58fead
reckafella <ethanmuthoniw6@gmail.com>
2024-01-15 17:25:35 +0300

    add simple api files

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -81,5 +81,6 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
         password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
         host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database=os.getenv('PERSONAL_DATA_DB_NAME'), port=3306)
+        database=os.getenv('PERSONAL_DATA_DB_NAME'),
+        port=3306)
     return connection
