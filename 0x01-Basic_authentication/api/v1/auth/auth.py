4cd1e6170c402aaa134cbfce1d866a3efd7f8ee1
Reckafella <ethanmuthoni@gmail.com>
2024-01-21 18:47:24 +0300

    update require_path

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 '''
 Auth module
 '''
+import fnmatch
 from flask import request
 from typing import List, TypeVar
 
@@ -14,11 +15,15 @@ class Auth():
         '''
         require auth method
         '''
-        if path is None or not excluded_paths:
+        if not path or not excluded_paths:
             return True
-        if not path.endswith('/'):
-            path += '/'
-        return path not in excluded_paths
+
+        path = path.rstrip('/') + '/'
+
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''
