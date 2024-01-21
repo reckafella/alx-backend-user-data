444ce080cbc27e8dcf79049c35b3006d99c7f205
Reckafella <ethanmuthoni@gmail.com>
2024-01-21 22:19:13 +0300

    Update the @app.before_request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,15 +32,16 @@ def before_request():
     '''
     before_request method
     '''
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth is None:
         return
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if not auth.authorization_header(request) and\
+            not auth.session_cookie(request):
         abort(401)
+
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
