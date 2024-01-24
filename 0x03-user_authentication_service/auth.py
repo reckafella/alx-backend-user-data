99573a7c1cf849de650062c3c5d3d8f9d28f4be7
Reckafella <ethanmuthoni@gmail.com>
2024-01-24 15:21:49 +0300

    add method for Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -36,3 +36,14 @@ class Auth:
             user = self._db.add_user(email, hashed_password)
             return user
         raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        '''
+        It should expect email and password (required arguments)\
+            and return a boolean.
+        '''
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
